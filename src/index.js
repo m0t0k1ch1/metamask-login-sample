4f6c290cabaf67fadc95b1bd44d16d16be98fc13
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-02-28 04:36:08 +0900

    check MetaMask condition

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import 'babel-polyfill'
+import Vue from 'vue/dist/vue.esm.js'
+import Eth from 'ethjs'
+
+let eth
+
+new Vue({
+  el: '#app',
+  data: {
+    errorMessage: null,
+  },
+  methods: {
+    login: function() {
+      var app = this
+
+      if (typeof web3 === 'undefined') {
+        alert('Please install MetaMask')
+        return
+      }
+
+      eth = new Eth(web3.currentProvider)
+
+      eth.accounts()
+        .then((accounts) => {
+          if (accounts.length <= 0) {
+            this.errorMessage = 'Please unlock MetaMask account'
+            throw this.errorMessage
+          }
+          return eth.net_version()
+        })
+        .then((netVersion) => {
+          if (netVersion !== '3') {
+            this.errorMessage = 'Please connect MetaMask to Ropsten Test Network'
+            throw this.errorMessage
+          }
+          alert('ok') // TODO
+        })
+        .catch((e) => {
+          if (this.errorMessage != null) {
+            alert(this.errorMessage)
+          }
+          throw e
+        })
+    },
+  },
+})
