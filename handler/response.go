2f13b3e20c4baf8ff521ad1e50cc3a27e0ea5fc3
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-04-10 19:36:08 +0900

    delete handler/helper.go

--- handler/response.go
+++ handler/response.go
@@ -1,6 +1,11 @@
 package handler
 
-import "github.com/m0t0k1ch1/metamask-login-sample/domain/model"
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/m0t0k1ch1/metamask-login-sample/domain/model"
+)
 
 const (
 	ResponseStateSuccess = "success"
@@ -24,4 +29,19 @@ func newErrorResponse(err *model.Error) *Response {
 		State:  ResponseStateError,
 		Result: err,
 	}
+}
+
+func jsonResponseSuccess(c echo.Context, result interface{}) error {
+	return c.JSON(http.StatusOK, newSuccessResponse(result))
+}
+
+func jsonResponseError(c echo.Context, err error) error {
+	var result *model.Error
+	if re, ok := err.(*model.Error); ok {
+		result = re
+	} else {
+		result = model.NewError(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, newErrorResponse(result))
 }
