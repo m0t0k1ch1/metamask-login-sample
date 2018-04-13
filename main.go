37cad35913a462b918cdb29dcba50ad20ed2f360
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-04-13 12:52:44 +0900

    enable to switch server port

--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"github.com/m0t0k1ch1/metamask-login-sample/application"
 	"github.com/m0t0k1ch1/metamask-login-sample/handler"
 	"github.com/m0t0k1ch1/metamask-login-sample/infrastructure/storage"
+)
+
+const (
+	DefaultServerPort = "1323"
 )
 
 func main() {
@@ -20,7 +25,16 @@ func main() {
 
 	setUpHandlers(e.Router())
 
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(":" + getPort()))
+}
+
+func getPort() string {
+	port := os.Getenv("MLS_SERVER_PORT")
+	if port == "" {
+		port = DefaultServerPort
+	}
+
+	return port
 }
 
 func setUpHandlers(router *echo.Router) {
