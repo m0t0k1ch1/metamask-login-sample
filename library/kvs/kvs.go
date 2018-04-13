4ef299048ea2153e3978d18b1448ad6d2fc52ee8
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-04-13 12:55:43 +0900

    [library/kvs] rename var : cache -> store

--- library/kvs/kvs.go
+++ library/kvs/kvs.go
@@ -4,20 +4,20 @@ import "sync"
 
 var (
 	mu    sync.RWMutex           = sync.RWMutex{}
-	cache map[string]interface{} = make(map[string]interface{})
+	store map[string]interface{} = make(map[string]interface{})
 )
 
 func Set(key string, value interface{}) {
 	mu.Lock()
 	defer mu.Unlock()
 
-	cache[key] = value
+	store[key] = value
 }
 
 func Get(key string) (value interface{}, ok bool) {
 	mu.RLock()
 	defer mu.RUnlock()
 
-	value, ok = cache[key]
+	value, ok = store[key]
 	return
 }
