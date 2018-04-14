066b3192943aa99927cc3294d825e00116325404
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2018-04-14 11:21:18 +0900

    use pointer

--- domain/auth_typed_data.go
+++ domain/auth_typed_data.go
@@ -23,14 +23,14 @@ func NewAuthTypedData(value string) *AuthTypedData {
 	}
 }
 
-func (data AuthTypedData) signatureHashBytes() []byte {
+func (data *AuthTypedData) signatureHashBytes() []byte {
 	return crypto.Keccak256(
 		crypto.Keccak256([]byte(data.Type+" "+data.Name)),
 		crypto.Keccak256([]byte(data.Value)),
 	)
 }
 
-func (data AuthTypedData) RecoverPubkey(sig Signature) (*Pubkey, error) {
+func (data *AuthTypedData) RecoverPubkey(sig Signature) (*Pubkey, error) {
 	pubkey, err := crypto.SigToPub(data.signatureHashBytes(), sig.Bytes())
 	if err != nil {
 		return nil, err
